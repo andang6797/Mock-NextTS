8d95020cbdda5b44b906d7b356bee84d6e77b6b8
AN-DANG\84886 <andang6797@gmail.com>
2023-02-12 21:51:41 +0700

    initial

---
+++ pages/home/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1> THIS IS HOME PAGE </h1>
+    </>
+  );
+};
+
+export default Home;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const Index: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/home");
+  }, []);
+  return (<></>);
+};
+
+export default Index;
